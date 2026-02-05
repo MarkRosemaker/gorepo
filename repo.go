de45e1e8727a6278a9f5d7a38e4f2e5217e97827
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2026-02-05 07:50:58 +0100

    basic go commands

--- repo.go
+++ repo.go
@@ -1,6 +1,7 @@
 package gorepo
 
 import (
+	"context"
 	"errors"
 	"io/fs"
 
@@ -18,4 +19,41 @@ func (r Repository) IsGoRepo() (bool, error) {
 	} else {
 		return false, err
 	}
+}
+
+// GoModInit initializes a go module in the repository.
+func (r Repository) GoModInit(ctx context.Context) error {
+	_, err := r.ExecCommand(ctx, "go", "mod", "init")
+	return err
+}
+
+// UpdateDependencies updates all dependencies in the repository.
+func (r Repository) UpdateDependencies(ctx context.Context) error {
+	if err := r.GoGetAll(ctx); err != nil {
+		return err
+	}
+
+	if err := r.GoModTidy(ctx); err != nil {
+		return err
+	}
+
+	return r.GoModVendor(ctx)
+}
+
+// GoGetAll updates all dependencies in the repository.
+func (r Repository) GoGetAll(ctx context.Context) error {
+	_, err := r.ExecCommand(ctx, "go", "get", "-u", "all")
+	return err
+}
+
+// GoModTidy tidies the go module in the repository.
+func (r Repository) GoModTidy(ctx context.Context) error {
+	_, err := r.ExecCommand(ctx, "go", "mod", "tidy")
+	return err
+}
+
+// GoModVendor vendors all dependencies in the repository.
+func (r Repository) GoModVendor(ctx context.Context) error {
+	_, err := r.ExecCommand(ctx, "go", "mod", "vendor")
+	return err
 }
