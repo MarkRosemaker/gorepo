651070e228c1cd579fe48242c203067fcb590f58
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2026-02-05 07:39:03 +0100

    add IsGoRepo method

--- repo.go
+++ repo.go
@@ -1,8 +1,21 @@
 package gorepo
 
 import (
+	"errors"
+	"io/fs"
+
 	"github.com/MarkRosemaker/ghrepo"
 )
 
 // Repository represents a local go repository.
 type Repository struct{ *ghrepo.Repository }
+
+func (r Repository) IsGoRepo() (bool, error) {
+	if _, err := r.Stat("go.mod"); err == nil {
+		return true, nil
+	} else if errors.Is(err, fs.ErrNotExist) {
+		return false, nil
+	} else {
+		return false, err
+	}
+}
