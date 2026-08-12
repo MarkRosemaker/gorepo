950d318e47f9a72663f9b2f9fad887deeb3125d0
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2026-08-12 20:48:16 +0200

    add GolangCILintFix

--- repo.go
+++ repo.go
@@ -130,7 +130,20 @@ func (r Repository) GoVet(ctx context.Context) error {
 }
 
 func (r Repository) GolangCILint(ctx context.Context) error {
-	if _, err := r.ExecCommand(ctx, "golangci-lint", "run", "./..."); err != nil {
+	return r.golangCILint(ctx, false)
+}
+
+func (r Repository) GolangCILintFix(ctx context.Context) error {
+	return r.golangCILint(ctx, true)
+}
+
+func (r Repository) golangCILint(ctx context.Context, fix bool) error {
+	args := []string{"run"}
+	if fix {
+		args = append(args, "--fix")
+	}
+
+	if _, err := r.ExecCommand(ctx, "golangci-lint", args...); err != nil {
 		const noPackagesMsg = "level=error msg=\"Running error: context loading failed: no go files to analyze: running `go mod tidy` may solve the problem\""
 		if execErr := (ghrepo.ExecError{}); errors.As(err, &execErr) &&
 			execErr.Out == noPackagesMsg {
